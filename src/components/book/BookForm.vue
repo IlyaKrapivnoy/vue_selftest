011d1e867a22e0b6637f54e6d85a8ad056b50455
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-08-02 19:06:09 +0100

    make input reusable

--- src/components/book/BookForm.vue
+++ src/components/book/BookForm.vue
@@ -2,15 +2,18 @@
   <div class="mt-10">
     <h2>Add a new book</h2>
     <form @submit.prevent class="flex flex-col w-full">
-      <input placeholder="Add title" v-model="book.title" />
-      <input placeholder="Add author" v-model="book.author" />
+      <my-input placeholder="Add title" v-model="book.title" />
+      <my-input placeholder="Add author" v-model="book.author" />
       <button class="mt-6" @click="createBook">Add</button>
     </form>
   </div>
 </template>
 
 <script>
+import MyInput from "@/components/UI/MyInput.vue";
+
 export default {
+  components: { MyInput },
   data() {
     return {
       book: {
--- src/components/shop/ShopForm.vue
+++ src/components/shop/ShopForm.vue
@@ -2,15 +2,18 @@
   <div class="mt-10">
     <h2>Add a new shop</h2>
     <form @submit.prevent class="flex flex-col w-full">
-      <input type="text" placeholder="Shop name" v-model="shop.shopName" />
-      <input type="text" placeholder="Location" v-model="shop.location" />
+      <my-input type="text" placeholder="Shop name" v-model="shop.shopName" />
+      <my-input type="text" placeholder="Location" v-model="shop.location" />
       <button @click="addShop" class="mt-6">Add</button>
     </form>
   </div>
 </template>
 
 <script>
+import MyInput from "@/components/UI/MyInput.vue";
+
 export default {
+  components: { MyInput },
   data() {
     return {
       shop: {
