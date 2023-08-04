d69f63d597b90fdc177dcf331a3dd76f9aba673b
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-08-04 18:01:19 +0100

    add animation

--- src/components/shop/ShopList.vue
+++ src/components/shop/ShopList.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="mt-10">
     <h2>Shop List</h2>
-    <shop-item
-      :shop="shop"
-      v-for="shop in shops"
-      :key="shop.id"
-      @removeShop="$emit('removeShop', shop)"
-    />
+    <transition-group name="shop-list">
+      <shop-item
+        :shop="shop"
+        v-for="shop in shops"
+        :key="shop.id"
+        @removeShop="$emit('removeShop', shop)"
+      />
+    </transition-group>
   </div>
 
   <div class="text-center mt-5" v-if="shops.length === 0">no items</div>
@@ -27,3 +29,20 @@ export default {
   emits: ["removeShop"],
 };
 </script>
+
+<style scoped>
+.shop-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+
+.shop-list-enter-active,
+.shop-list-leave-active {
+  transition: all 0.4s ease;
+}
+.shop-list-enter-from,
+.shop-list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+</style>
