6905b6d7100a31f68fa36e895cc8414f1c3a073a
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-08-05 18:22:26 +0100

    add back btn

--- src/views/SingleBook.vue
+++ src/views/SingleBook.vue
@@ -2,6 +2,8 @@
   <div class="container mx-auto px-4">
     <h1>Book: {{ bookTitle }}</h1>
     <p>Book description will be here...</p>
+
+    <button @click="handleBackButton">Back</button>
   </div>
 </template>
 
@@ -15,6 +17,11 @@ export default {
   },
   created() {
     this.bookTitle = this.$route.params.title;
+  },
+  methods: {
+    handleBackButton() {
+      this.$router.push("/bookshelf");
+    },
   },
 };
 </script>
