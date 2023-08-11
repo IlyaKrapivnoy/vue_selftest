cb0336bd9c642121e2dfc9465c7cd6cffaf63cfe
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-08-11 16:52:18 +0100

    add reusable-input

---
+++ src/components/partials/ReusableInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <label :for="inputId" class="text-lg font-bold">{{ label }}</label>
+    <el-input
+      :placeholder="placeholder"
+      v-model="internalValue"
+      :id="inputId"
+      clearable
+    ></el-input>
+  </div>
+</template>
+
+<script>
+import { ref, watch } from "vue";
+
+export default {
+  props: {
+    label: String,
+    placeholder: String,
+    inputId: String,
+    inputValue: String,
+  },
+  emits: ["update:inputValue"],
+  setup(props, { emit }) {
+    const internalValue = ref(props.inputValue);
+
+    watch(internalValue, (newValue) => {
+      emit("update:inputValue", newValue);
+    });
+
+    return {
+      internalValue,
+    };
+  },
+};
+</script>
--- src/views/Todo/TodoView.vue
+++ src/views/Todo/TodoView.vue
@@ -1,9 +1,43 @@
 <template>
   <main class="container mx-auto px-4 mt-20">
-    <h1>TODO</h1>
+    <h1>TODO PAGE</h1>
+    <form @submit.prevent="handleSubmit" class="flex flex-col">
+      <reusable-input
+        label="Add new todo"
+        placeholder="Enter your task..."
+        inputId="todoInput"
+        @update:inputValue="input = $event"
+        class="mt-6"
+      />
+
+      <el-button
+        native-type="submit"
+        type="primary"
+        class="self-end w-1/5 mt-3"
+      >
+        Add
+      </el-button>
+    </form>
   </main>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+import ReusableInput from "@/components/partials/ReusableInput.vue";
+
+export default {
+  components: { ReusableInput },
+  setup() {
+    const input = ref("");
+
+    const handleSubmit = () => {
+      console.log(input.value);
+    };
+
+    return {
+      input,
+      handleSubmit,
+    };
+  },
+};
 </script>
