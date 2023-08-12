c97c501c54121a962294194af31fa2377a5554ae
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-08-12 19:16:59 +0100

    change data

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -1,14 +1,60 @@
 const camusQuotes = [
-  "Don't walk in front of me; I may not follow. Don't walk behind me; I may not lead. Just walk beside me and be my friend.",
-  "In the midst of winter, I found there was, within me, an invincible summer.",
-  "The only way to deal with this life is to find meaning in it.",
-  "Man is the only creature who refuses to be what he is.",
-  "There is no sun without shadow, and it is essential to know the night.",
-  "Live to the point of tears.",
-  "Freedom is nothing but a chance to be better.",
-  "The struggle itself towards the heights is enough to fill a man's heart. One must imagine Sisyphus happy.",
-  "Real generosity towards the future lies in giving all to the present.",
-  "You will never be happy if you continue to search for what happiness consists of. You will never live if you are looking for the meaning of life.",
+  {
+    quote:
+      "Don't walk in front of me; I may not follow. Don't walk behind me; I may not lead. Just walk beside me and be my friend.",
+    author: "A. Camus",
+    category: "philosophy",
+  },
+  {
+    quote:
+      "In the midst of winter, I found there was, within me, an invincible summer.",
+    author: "A. Camus",
+    category: "philosophy",
+  },
+  {
+    quote: "The only way to deal with this life is to find meaning in it.",
+    author: "A. Camus",
+    category: "philosophy",
+  },
+  {
+    quote: "Man is the only creature who refuses to be what he is.",
+    author: "A. Camus",
+    category: "philosophy",
+  },
+  {
+    quote:
+      "There is no sun without shadow, and it is essential to know the night.",
+    author: "A. Camus",
+    category: "philosophy",
+  },
+  {
+    quote: "Live to the point of tears.",
+    author: "A. Camus",
+    category: "philosophy",
+  },
+  {
+    quote: "Freedom is nothing but a chance to be better.",
+    author: "A. Camus",
+    category: "philosophy",
+  },
+  {
+    quote:
+      "The struggle itself towards the heights is enough to fill a man's heart. One must imagine Sisyphus happy.",
+    author: "A. Camus",
+    category: "philosophy",
+  },
+  {
+    quote:
+      "Real generosity towards the future lies in giving all to the present.",
+    author: "A. Camus",
+    category: "philosophy",
+  },
+  {
+    quote:
+      "You will never be happy if you continue to search for what happiness consists of. You will never live if you are looking for the meaning of life.",
+    author: "A. Camus",
+    category: "philosophy",
+  },
 ];
 
 export default camusQuotes;
--- src/views/Quotes/partials/QuoteDisplay.vue
+++ src/views/Quotes/partials/QuoteDisplay.vue
@@ -1,6 +1,7 @@
 <template>
   <el-card v-if="quote" class="box-card mt-6">
-    <p class="italic">{{ quote }}</p>
+    <p class="italic">{{ quote.quote }}</p>
+    <p>{{ quote.author }}</p>
   </el-card>
 </template>
 
