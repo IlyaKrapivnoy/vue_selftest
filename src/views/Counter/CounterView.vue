df2eed8db71c5a563280980af41b0b6ed8ff6226
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-08-15 17:06:00 +0100

    add alert

--- src/views/Counter/CounterView.vue
+++ src/views/Counter/CounterView.vue
@@ -4,13 +4,31 @@
     <div
       class="flex flex-col justify-center items-center h-[calc(100vh-300px)]"
     >
-      <div class="text-[100px] font-bold text-orange-500">
+      <div class="text-[100px] font-bold text-gray-600">
         {{ counter }}
       </div>
-      <div>
-        <el-button @click="decrease" type="success" plain>-</el-button>
-        <el-button @click="increase" type="success" plain>+</el-button>
-        <el-button @click="reset" type="success" plain>reset</el-button>
+      <div class="w-[200px] flex flex-col items-center">
+        <div class="w-full flex">
+          <el-button @click="decrease" type="success" plain class="w-1/2">
+            -
+          </el-button>
+          <el-button @click="increase" type="success" plain class="w-1/2">
+            +
+          </el-button>
+        </div>
+        <el-button @click="reset" type="danger" plain class="w-full mt-4">
+          reset
+        </el-button>
+      </div>
+
+      <div v-show="isAlert" class="alert">
+        <el-alert
+          title="warning alert"
+          type="warning"
+          description="Only positive numbers"
+          show-icon
+          :closable="false"
+        />
       </div>
     </div>
   </main>
@@ -21,12 +39,36 @@ import { ref } from "vue";
 export default {
   setup() {
     const counter = ref(100);
+    let isAlert = ref(false);
 
-    const decrease = () => counter.value--;
-    const increase = () => counter.value++;
-    const reset = () => (counter.value = 0);
+    const decrease = () => {
+      console.log("isAlert:", isAlert);
+      if (counter.value > 0) {
+        counter.value--;
+      } else {
+        isAlert.value = true;
+      }
+    };
 
-    return { counter, decrease, increase, reset };
+    const increase = () => {
+      counter.value++;
+      isAlert.value = false;
+    };
+    const reset = () => {
+      counter.value = 0;
+      isAlert.value = false;
+    };
+
+    return { counter, isAlert, decrease, increase, reset };
   },
 };
 </script>
+
+<style scoped>
+.alert {
+  position: fixed;
+  width: 45%;
+  bottom: 20%;
+  right: 6%;
+}
+</style>
