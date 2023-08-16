04067c4cdd18ab6c505d1903f7a165f1977956ea
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-08-16 16:16:36 +0100

    adjust likes

--- src/views/Quotes/partials/QuoteDisplay.vue
+++ src/views/Quotes/partials/QuoteDisplay.vue
@@ -1,19 +1,35 @@
 <template>
   <el-card v-if="quote" class="box-card mt-6">
-    <div>
-      <p class="italic">{{ quote.quote }}</p>
-      <p>{{ quote.author }}</p>
-    </div>
-    <div class="flex">
-      <p>Likes: <span>-</span> {{ quote.likes }} <span>+</span></p>
+    <div class="flex flex-col">
+      <div>
+        <p class="italic">{{ quote.quote }}</p>
+        <p>{{ quote.author }}</p>
+      </div>
+      <div class="flex self-end items-center">
+        <el-button @click="incrementLikes" type="success" plain round
+          >+</el-button
+        >
+        <span class="px-3">
+          {{ quote.likes }}
+        </span>
+        <el-button @click="decrementLikes" type="success" plain round
+          >-</el-button
+        >
+      </div>
     </div>
   </el-card>
 </template>
 
-<script>
-export default {
-  props: {
-    quote: Object,
-  },
+<script setup>
+const decrementLikes = () => {
+  console.log("decrementLikes", --props.quote.likes);
 };
+
+const incrementLikes = () => {
+  console.log("incrementLikes", ++props.quote.likes);
+};
+
+const props = defineProps({
+  quote: Object,
+});
 </script>
