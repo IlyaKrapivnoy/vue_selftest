fb02f299d677f5d7343e5564783b572b494d6c0e
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-08-17 17:10:41 +0100

    remove unused files

--- src/components/index.js
+++ src/components/index.js
@@ -1,19 +1,4 @@
-import MyInput from "@/components/utils/MyInput.vue";
-import MyDialog from "@/components/utils/MyDialog.vue";
 import MySpinner from "@/components/common/MySpinner.vue";
 import MyPagination from "@/components/common/MyPagination.vue";
-import MyItem from "@/components/utils/MyItem.vue";
-import MyItemList from "@/components/utils/MyItemList.vue";
-import MyForm from "@/components/utils/MyForm.vue";
-import MyNavigation from "@/components/layout/MainHeader.vue";
 
-export default [
-  MyInput,
-  MyDialog,
-  MySpinner,
-  MyPagination,
-  MyItem,
-  MyItemList,
-  MyForm,
-  MyNavigation,
-];
+export default [MySpinner, MyPagination];
