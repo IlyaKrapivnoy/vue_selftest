2768ac32eb31517d398e75c78759a30ad9b78138
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-08-20 21:36:31 +0100

    test footer

---
+++ src/components/layout/Footer/__tests__/MainFooter.test.js
@@ -0,0 +1,12 @@
+import { mount } from "@vue/test-utils";
+import Footer from "@/components/layout/Footer/MainFooter.vue";
+
+describe("Footer.vue", () => {
+  it("displays the current year", () => {
+    const wrapper = mount(Footer);
+
+    const currentYear = new Date().getFullYear();
+    expect(wrapper.text()).toContain(`By Ilya Krapivnoy ${currentYear}`);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
