d9453dff4de621f14dd99690aab0698a9bd12e59
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-08-22 13:46:16 +0100

    expand head setter test cases

--- src/components/common/HeadSetter/__tests__/HeadSetter.test.js
+++ src/components/common/HeadSetter/__tests__/HeadSetter.test.js
@@ -21,4 +21,33 @@ describe("HeadSetter", () => {
       meta: [{ name: name, content: content }],
     });
   });
+
+  it("renders nothing in the DOM", () => {
+    const wrapper = mount(HeadSetter);
+
+    expect(wrapper.html()).toBe("");
+  });
+
+  it("renders nothing in the DOM when props are empty", () => {
+    const wrapper = mount(HeadSetter, {
+      props: { title: "", name: "", content: "" },
+    });
+
+    expect(wrapper.html()).toBe("");
+  });
+
+  it("calls useHead with provided props", () => {
+    const title = "Page Title";
+    const name = "description";
+    const content = "This is the page description";
+
+    mount(HeadSetter, {
+      props: { title, name, content },
+    });
+
+    expect(useHead).toHaveBeenCalledWith({
+      title: title,
+      meta: [{ name: name, content: content }],
+    });
+  });
 });
