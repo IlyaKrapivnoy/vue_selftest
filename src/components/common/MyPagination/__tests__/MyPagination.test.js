c7b0e67a883231db99c258cb576631fc2038bd2c
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-08-22 13:38:10 +0100

    expand pagination test cases

--- src/components/common/MyPagination/__tests__/MyPagination.test.js
+++ src/components/common/MyPagination/__tests__/MyPagination.test.js
@@ -26,4 +26,42 @@ describe("MyPagination", () => {
     expect(wrapper.emitted("page-change")).toBeTruthy();
     expect(wrapper.emitted("page-change")[0][0]).toBe(4);
   });
+
+  it("correctly renders the last page", () => {
+    const wrapper = mount(MyPagination, {
+      props: {
+        page: 10,
+        totalPages: 10,
+      },
+    });
+
+    const lastPageElement = wrapper.find(".page-item:last-child");
+    expect(lastPageElement.text()).toBe("10");
+  });
+
+  it("correctly highlights the current page", () => {
+    const wrapper = mount(MyPagination, {
+      props: {
+        page: 3,
+        totalPages: 7,
+      },
+    });
+
+    const currentPageItem = wrapper.find(".page-item.current-page");
+    expect(currentPageItem.exists()).toBe(true);
+  });
+
+  it("handles page-change event correctly", async () => {
+    const wrapper = mount(MyPagination, {
+      props: {
+        page: 3,
+        totalPages: 7,
+      },
+    });
+
+    await wrapper.vm.changePage(5);
+
+    expect(wrapper.emitted("page-change")).toBeTruthy();
+    expect(wrapper.emitted("page-change")[0][0]).toBe(5);
+  });
 });
