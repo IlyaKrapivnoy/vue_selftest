d508f86fdfaedfc0025cc71421b1c5e676de3212
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-08-22 13:55:22 +0100

    test btns

--- src/components/common/CustomCard/__tests__/CustomCard.test.js
+++ src/components/common/CustomCard/__tests__/CustomCard.test.js
@@ -16,4 +16,24 @@ describe("CustomCard", () => {
 
     expect(wrapper.html()).toMatchSnapshot();
   });
+
+  it("renders buttons with correct types and names", () => {
+    const cardTextBold = "Bold Text";
+    const cardTextLight = "Light Text";
+    const buttons = [
+      { type: "primary", name: "Button 1", click: jest.fn() },
+      { type: "success", name: "Button 2", click: jest.fn() },
+    ];
+
+    const wrapper = shallowMount(CustomCard, {
+      props: { cardTextBold, cardTextLight, buttons },
+    });
+
+    const buttonWrappers = wrapper.findAll(".el-button");
+    buttonWrappers.forEach((buttonWrapper, index) => {
+      const button = buttons[index];
+      expect(buttonWrapper.classes()).toContain(`el-button--${button.type}`);
+      expect(buttonWrapper.text()).toBe(button.name);
+    });
+  });
 });
