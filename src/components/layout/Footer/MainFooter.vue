7455523d41ad244a2c5e8f5d4bcca3e6c986ce21
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-08-24 18:54:34 +0100

    use setup for footer

--- src/components/layout/Footer/MainFooter.vue
+++ src/components/layout/Footer/MainFooter.vue
@@ -6,15 +6,8 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import { ref } from "vue";
 
-export default {
-  setup() {
-    const currentYear = ref(new Date().getFullYear());
-    return {
-      currentYear,
-    };
-  },
-};
+const currentYear = ref(new Date().getFullYear());
 </script>
