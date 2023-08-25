88fd719b3e4bbc8bd937d2686312b34d2b2680b3
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-08-25 18:08:05 +0100

    change button controller section

--- src/views/Counter/partials/ButtonControllerSection.vue
+++ src/views/Counter/partials/ButtonControllerSection.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="w-full flex">
-    <el-button @click="$emit('decrease')" type="success" plain class="w-1/2">
+    <el-button @click="$emit('decrease')" type="success" plain class="w-1/3">
       -
     </el-button>
-    <el-button @click="$emit('increase')" type="success" plain class="w-1/2">
+    <el-button @click="$emit('increase')" type="success" plain class="w-1/3">
       +
     </el-button>
+    <el-button @click="$emit('reset')" type="danger" plain class="w-1/3">
+      reset
+    </el-button>
   </div>
-  <el-button @click="$emit('reset')" type="danger" plain class="w-full mt-4">
-    reset
-  </el-button>
 </template>
