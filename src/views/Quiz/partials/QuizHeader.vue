455fd4f373f8ebb6a73c204c3aa5df50b61c8b1d
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-09-16 23:32:56 +0100

    change quiz header

--- src/views/Quiz/partials/QuizHeader.vue
+++ src/views/Quiz/partials/QuizHeader.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="flex justify-between text-indigo-300">
-    <span>{{ props.getCurrentQuestion.question }}</span>
+  <div class="flex justify-between items-center h-[50px] mt-3 text-indigo-300">
+    <span
+      class="text-green-600 font-bold uppercase mr-10 pl-4 border-green-600 border-l-4"
+      >{{ props.getCurrentQuestion.question }}</span
+    >
     <div class="w-[240px] flex justify-between">
-      <span
-        >Score {{ props.currentScore }}/{{ props.totalQuestionsCount }}</span
-      >
-      <span class="border-r-[1px] border-black"></span>
-      <span
-        >Question {{ props.totalQuestionsCount }}/{{
-          props.getCurrentQuestion.index + 1
-        }}</span
-      >
+      <p>
+        <span class="font-bold">Score:</span> {{ props.currentScore }}/{{
+          props.totalQuestionsCount
+        }}
+      </p>
+      <span class="border-r-[1px] border-dotted border-black"></span>
+      <p>
+        <span class="font-bold">Question:</span>
+        {{ props.getCurrentQuestion.index + 1 }}/{{ props.totalQuestionsCount }}
+      </p>
     </div>
   </div>
 </template>
