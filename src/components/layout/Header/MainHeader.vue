4af3d3a03df128b890099a03975fd6fbba45de60
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-09-17 00:11:03 +0100

    change logo and its styles

--- src/components/layout/Header/MainHeader.vue
+++ src/components/layout/Header/MainHeader.vue
@@ -2,14 +2,22 @@
   <main class="container mx-auto">
     <el-menu
       :default-active="activeIndex.toString()"
-      class="el-menu-demo flex justify-between uppercase"
+      class="el-menu-demo flex justify-between items-center uppercase h-[80px]"
       mode="horizontal"
     >
       <router-link to="/" @click="setActiveIndex(0)">
         <el-menu-item index="1" class="logo-nav-item">
-          <div class="flex flex-col items-center cursor-pointer ml-4">
-            <div class="text-3xl font-bold text-indigo-300">Logo</div>
-            <div class="text-xs text-gray-500">practice inc.</div>
+          <div class="flex flex-col items-center cursor-pointer ml-4 mb-2">
+            <div class="text-3xl font-bold text-indigo-300">
+              <img
+                src="../../../../public/logo.png"
+                alt="logo"
+                class="w-[80px]"
+              />
+            </div>
+            <div class="text-[10px] text-gray-500 -mt-3 leading-4">
+              practice inc.
+            </div>
           </div>
         </el-menu-item>
       </router-link>
@@ -44,7 +52,16 @@ onMounted(() => {
 .logo-nav-item {
   padding-left: 0 !important;
 }
+
 .logo-nav-item:hover {
   background-color: transparent !important;
+}
+
+.el-sub-menu .el-icon {
+  height: 80px;
+}
+
+.el-sub-menu__title {
+  padding-right: 20px !important;
 }
 </style>
