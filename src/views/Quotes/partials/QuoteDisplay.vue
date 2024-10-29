666b8262759e1e13670efb13fd57a6ab8c0d751f
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2024-10-29 10:53:07 +0100

    allow only 1 like/dislike click

--- src/views/Quotes/partials/QuoteDisplay.vue
+++ src/views/Quotes/partials/QuoteDisplay.vue
@@ -6,20 +6,36 @@
         <p>{{ props.quote.author }}</p>
       </div>
       <div class="flex self-end items-center">
-        <el-button @click="$emit('increaseLikes')" type="success" plain round
-          >+</el-button
+        <el-button
+          :plain="!store.state.quotes.likeStatus"
+          @click="$emit('increaseLikes')"
+          type="success"
+          round
         >
-        <span class="px-3"> {{ props.quote.likes }} </span>
-        <el-button @click="$emit('decreaseLikes')" type="success" plain round
-          >-</el-button
+          +
+        </el-button>
+        <span class="px-3">{{ props.quote.likes }}</span>
+        <el-button
+          :plain="!store.state.quotes.dislikeStatus"
+          @click="$emit('decreaseLikes')"
+          type="success"
+          round
         >
+          -
+        </el-button>
       </div>
     </div>
   </el-card>
 </template>
 
 <script setup>
+import { useStore } from "vuex";
+
 const props = defineProps({
   quote: Object,
 });
+/* eslint-disable */
+const emits = defineEmits(["increaseLikes", "decreaseLikes"]);
+
+const store = useStore();
 </script>
