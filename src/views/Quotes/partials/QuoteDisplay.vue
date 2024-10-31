67d2111d48458d93223647d198d989c33709c555
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2024-10-31 12:01:06 +0100

    change increment / decrement buttons to icon

--- src/views/Quotes/partials/QuoteDisplay.vue
+++ src/views/Quotes/partials/QuoteDisplay.vue
@@ -5,25 +5,14 @@
         <p class="italic">{{ props.quote.quote }}</p>
         <p>{{ props.quote.author }}</p>
       </div>
-      <div class="flex self-end items-center">
-        <el-button
-          :plain="!store.state.quotes.likeStatus"
-          @click="$emit('increaseLikes')"
-          type="success"
-          round
-        >
-          +
-        </el-button>
+      <div
+        @click="$emit('increaseLikes')"
+        class="flex self-end items-center cursor-pointer"
+      >
+        <SvgLikeIcon
+          :class="{ liked: store.state.quotes.likeStatus ? 'liked' : '' }"
+        />
         <span class="px-3">{{ props.quote.likes }}</span>
-        <el-button
-          :plain="!store.state.quotes.dislikeStatus"
-          @click="$emit('decreaseLikes')"
-          type="success"
-          round
-        >
-          -
-        </el-button>
-        <img src="@/assets/LikeIcon.svg" alt="Logo" />
       </div>
     </div>
   </el-card>
@@ -31,12 +20,19 @@
 
 <script setup>
 import { useStore } from "vuex";
+import SvgLikeIcon from "@/svg/SvgLikeIcon.vue";
 
 const props = defineProps({
   quote: Object,
 });
+
 /* eslint-disable */
 const emits = defineEmits(["increaseLikes", "decreaseLikes"]);
-
 const store = useStore();
 </script>
+
+<style scoped>
+.liked {
+  fill: red;
+}
+</style>
